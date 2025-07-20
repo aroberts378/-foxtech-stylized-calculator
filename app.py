902217a9e148ccbcca15dc18709f430a4b930ef1
ggfoxtech758 <ggfoxtech758@gmail.com>
2025-07-19 23:49:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Use PORT env if available
+    app.run(host='0.0.0.0', port=port)        # No debug mode
